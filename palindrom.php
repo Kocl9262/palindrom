5e61b6bfe17d0062388943722d116cb2fa969ae9
Sebastjan Koci <sebastjan.koci@gmail.com>
2016-02-13 16:27:15 +0100

    dodan utf8

--- palindrom.php
+++ palindrom.php
@@ -1,12 +1,18 @@
 <?php
 
 	$string = $_POST["string"];
+
+    function utf8_strrev($str)
+    {
+        preg_match_all('/./us', $str, $ar);
+        return join('', array_reverse($ar[0]));
+    }
 	
 	function palindrom($string)
 	{
 		$string = str_replace(" ", "", $string);
 		
-		if ($string == strrev($string))
+		if ($string == utf8_strrev($string))
 		{
 			return true;
 		}
